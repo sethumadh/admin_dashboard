54df35c1193e9d64de1ff783951d516ac2e79f19
Sethumadhavan Kochukrishnan potti <sethumadhavankochukrishnanpotti@Sethumadhavans-MacBook-Pro.local>
2023-07-06 14:20:48 +1000

    mui settings with expanded theme types

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+import { useMemo } from "react"
+import { themeSettings } from "@/theme"
+
 function App() {
-  return <div className="app" style={{backgroundColor:"white"}}>hello</div>
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
+    </div>
+  )
 }
 
 export default App
