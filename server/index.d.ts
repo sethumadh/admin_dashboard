0c2c96945e34042542d53e97e6e5aa03664bed76
Sethumadhavan Kochukrishnan potti <sethumadhavankochukrishnanpotti@Sethumadhavans-MacBook-Pro.local>
2023-08-09 12:29:46 +1000

    add vercel.json

--- server/index.d.ts
+++ server/index.d.ts
@@ -1,6 +1,6 @@
 import { Express } from 'express-serve-static-core';
 
-import { IUserModel } from './src/models/User';
+import { IUserModel } from './api/models/User';
 //  This same code I have reproduced on the files that required an extended req.user is required
 declare module 'express-serve-static-core' {
     interface Request {
